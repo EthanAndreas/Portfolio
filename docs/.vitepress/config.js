e224b74e40b8d3cd552d3044829e63b51b79ffe6
EthanAndreas <ethanhuret27@gmail.com>
2023-06-25 14:00:59 +0200

    fix link and new rule

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -45,11 +45,8 @@ export default defineConfig({
     ],
 
     footer: {
-      links: [
-        { text: 'Privacy Policy', link: '/privacy' }, // Add this line
-      ],
       message: 'Released under the GNU General Public License.',
-      copyright: '© 2023 Ethan Huret, all rights reserved. <a href=https://ethanandreas.github.io/Portfolio/policy.html>Privacy policy.</a.html>',
+      copyright: '© 2023 Ethan Huret, all rights reserved. <a href=/Portfolio/policy>Privacy policy.</a>',
     },
   }
 })
